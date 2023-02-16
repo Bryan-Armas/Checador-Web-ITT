b1a46e49b6fddbdbca1e11337abc85145a2e2746
Bryan-Armas <bryan.armas18@tectijuana.edu.mx>
2023-02-16 10:15:13 -0800

    update

--- formu.php
+++ formu.php
@@ -1,22 +1,22 @@
 <?php
-$Nom = $_POST['PERCVE'];
-$Ape = $_POST['PERAPE'];
-$Tel = $_POST['PERNOM'];
-$Nom = $_POST['PDOCVE'];
-$Ape = $_POST['LUNHRA'];
-$Tel = $_POST['LUNAUL'];
-$Ape = $_POST['MARHRA'];
-$Tel = $_POST['MARAUL'];
-$Ape = $_POST['MIEHRA'];
-$Tel = $_POST['MIEAUL'];
-$Ape = $_POST['JUEHRA'];
-$Tel = $_POST['JUEAUL'];
-$Ape = $_POST['VIEHRA'];
-$Tel = $_POST['VIEAUL'];
-$Ape = $_POST['SABHRA'];
-$Tel = $_POST['SABAUL'];
-$Ape = $_POST['DOMHRA'];
-$Tel = $_POST['DOMAUL'];
+$PERCVE = $_POST['PERCVE'];
+$PERAPE = $_POST['PERAPE'];
+$PERNOM = $_POST['PERNOM'];
+$PDOCVE = $_POST['PDOCVE'];
+$LUNHRA = $_POST['LUNHRA'];
+$LUNAUL = $_POST['LUNAUL'];
+$MARHRA = $_POST['MARHRA'];
+$MARAUL = $_POST['MARAUL'];
+$MIEHRA = $_POST['MIEHRA'];
+$MIEAUL = $_POST['MIEAUL'];
+$JUEHRA = $_POST['JUEHRA'];
+$JUEAUL = $_POST['JUEAUL'];
+$VIEHRA = $_POST['VIEHRA'];
+$VIEAUL = $_POST['VIEAUL'];
+$SABHRA = $_POST['SABHRA'];
+$SABAUL = $_POST['SABAUL'];
+$DOMHRA = $_POST['DOMHRA'];
+$DOMAUL = $_POST['DOMAUL'];
 
 if(!empty($PERCVE) || !empty($PERAPE) || !empty($PERNOM) || !empty($PDOCVE) || !empty($LUNHRA) || !empty($LUNAUL) || !empty($MARHRA) || !empty($MARAUL) || !empty($MIEHRA) || !empty($MIEAUL) 
     || !empty($JUEHRA) || !empty($JUEAUL) || !empty($VIEHRA) || !empty($VIEAUL) || !empty($SABHRA) || !empty($SABAUL) || !empty($DOMHRA) || !empty($DOMAUL) ){
@@ -30,19 +30,21 @@ if(!empty($PERCVE) || !empty($PERAPE) || !empty($PERNOM) || !empty($PDOCVE) || !
         die('connect error('.mysqli_connect_error().')'.mysqli_connect_error());
     }
     else{
-        $INSERT = "INSERT INTO empleados1 (IdEmpleados,Nom,Ape,Tel)
-        values(?,?,?,?)";
+        $SELECT = "SELECT PERCVE from bd where PERCVE = ? limit 1 ";
+        $INSERT = "INSERT INTO bd (PERCVE,PERAPE,PERNOM,PDOCVE,LUNHRA,LUNAUL,MARHRA,MARAUL,MIEHRA,MIEAUL,JUEHRA,JUEAUL,VIEHRA,VIEAUL,SABHRA,SABAUL,DOMHRA,DOMAUL)
+        values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 
         $stmt = $conn->prepare($SELECT);
-        $stmt ->bind_param( "i", $Tel);
+        $stmt ->bind_param( "i", $PERCVE);
         $stmt ->execute();
-        $stmt ->bind_result($Tel);
+        $stmt ->bind_result($PERCVE);
         $stmt ->store_result();
         $rnum =$stmt->num_rows;
         if($rnum == 0){
             $stmt ->close();
             $stmt = $conn->prepare($INSERT);
-            $stmt ->bind_param( "issi", $IdEmpleados,$Nom,$Ape,$Tel);
+            $stmt ->bind_param( "iiiiisisisisisisis", $PERCVE,$PERAPE,$PERNOM,$PDOCVE,$LUNHRA,$LUNAUL,$MARHRA,$MARAUL,$MIEHRA,$MIEAUL,$JUEHRA,$JUEAUL,$VIEHRA,$VIEAUL,$SABHRA,$SABAUL,
+            $DOMHRA,$DOMAUL);
             $stmt ->execute();
             echo "Registro Completado";
         }
